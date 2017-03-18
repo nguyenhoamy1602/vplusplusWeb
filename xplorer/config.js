03504f4cac7fe874db7853fd31ab189ebd10c50b
Nam Nguyen <namn1@student.unimelb.edu.au>
2017-03-18 14:23:45 +1100

    remove nodejs

--- xplorer/config.js
+++
@@ -1,8 +0,0 @@
-var config = {}
-
-config.host = process.env.HOST || "https://vplusplus.documents.azure.com:443/";
-config.authKey = process.env.AUTH_KEY || "aLSJt0fu1cGEpFNvOQtBgbaZ4if7SfwEpka4Co29WLS74KRunJLkUTcTAPHEHMUxbS6iHW7MfyNHTmf4EPGk7w==";
-config.databaseId = "ToDoList";
-config.collectionId = "Items";
-
-module.exports = config;
