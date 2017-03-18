86b37fb31db914ebedd335c3de0e1bb1a725b73e
Nam Nguyen <namn1@student.unimelb.edu.au>
2017-03-18 14:55:22 +1100

    flask server

--- index.py
+++ index.py
@@ -1,12 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
-
+app = Flask(__name__, static_folder='.', static_url_path='')
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+def index():
+    return render_template("/index.html")
 
 if __name__ == '__main__':
     app.run()
